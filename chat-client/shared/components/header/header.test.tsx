28fbd9ca4217a0c8a13c0737b4ac85206e1c9f30
Renate <renate.gouveia@gmail.com>
2019-08-03 23:26:44 +0800

    Remove unused uncommented code

--- chat-client/shared/components/header/header.test.tsx
+++ chat-client/shared/components/header/header.test.tsx
@@ -23,27 +23,4 @@ describe('<Header />', () => {
     const wrapper = shallow(<Header links={links} />);
     expect(wrapper.find('#link-0').length).toEqual(1);
   });
-
-  // it('should set the email value on change event', () => {
-  //   wrapper.find('#email').simulate('change', {
-  //     persist: jest.fn(),
-  //     target: {
-  //       id: 'email',
-  //       value: 'freyo@gmail.com',
-  //     },
-  //   });
-  //   expect(wrapper.find('#email').prop('value')).toEqual('freyo@gmail.com');
-  // });
-
-  // it('should display errors under fields if blank', () => {
-  //   wrapper.find('#login-form').simulate('submit', {
-  //     preventDefault: jest.fn(),
-  //   });
-  //   expect(wrapper.find('#email-error').contains('Warrior! We must know your name for the battle field!'));
-  //   expect(wrapper.find('#password-error').contains('You must provide this, for passage!'));
-  // });
-
-  // it('should display server error if "error" prop is passed', () => {
-  //   expect(wrapper.find('#login-server-error').contains('Server error'));
-  // });
 });
