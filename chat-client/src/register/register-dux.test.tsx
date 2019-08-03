fe40f0db6e0110870f7140dc2dd7fe5afa96cdc6
Renate <renate.gouveia@gmail.com>
2019-08-04 02:58:25 +0800

    Add reducer tests for login and register

--- chat-client/src/register/register-dux.test.tsx
+++ chat-client/src/register/register-dux.test.tsx
@@ -1,5 +1,5 @@
-
-import { RegisterActionType, registerError  } from './register-dux';
+import { initialState, registerReducer, RegisterActionType, registerError } from './register-dux';
+import { Action } from 'redux';
 
 describe('Login Actions', () => {
   it('should create an action: registerError', () => {
@@ -9,5 +9,53 @@ describe('Login Actions', () => {
       payload: errorText,
     };
     expect(registerError(errorText)).toEqual(expectedAction);
+  });
+});
+
+const action: Action<any> = { type: '' };
+describe('Register Reducer', () => {
+  it('should return the initial state', () => {
+    expect(registerReducer(initialState, action)).toEqual(initialState);
+  });
+
+  it('should handle FETCH REQUEST', () => {
+    expect(
+      registerReducer(initialState, {
+        type: RegisterActionType.FETCH_REQUEST,
+      }),
+    ).toEqual({
+      data: {},
+      error: '',
+      loading: true,
+      success: false,
+    });
+  });
+
+  it('should handle FETCH SUCCESS', () => {
+    const mockData = {
+      email: 'renate',
+      token: '1234',
+    };
+    expect(
+      registerReducer(initialState, {
+        type: RegisterActionType.FETCH_SUCCESS,
+        payload: mockData,
+      }),
+    ).toEqual({
+      ...initialState,
+      data: mockData,
+    });
+  });
+
+  it('should handle FETCH ERROR', () => {
+    expect(
+      registerReducer(initialState, {
+        type: RegisterActionType.FETCH_ERROR,
+        payload: 'Error has occured',
+      }),
+    ).toEqual({
+      ...initialState,
+      error: 'Error has occured',
+    });
   });
 });
