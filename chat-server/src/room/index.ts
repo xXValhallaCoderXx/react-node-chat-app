dd13de67fc982c8ac9401e73cf62126107e72f1a
Renate <renate.gouveia@gmail.com>
2019-08-24 14:36:48 +0800

    Add room entitiy test

--- chat-server/src/room/index.ts
+++ chat-server/src/room/index.ts
@@ -2,3 +2,4 @@ export { default as RoomController } from './controller';
 export { default as RoomServices } from './services';
 export { Room as RoomType } from './interface';
 export {default as RoomModel} from "./model";
+export {default as RoomEntity} from "./entity";
